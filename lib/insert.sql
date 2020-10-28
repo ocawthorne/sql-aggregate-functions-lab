ba1eb74e92c8b04c41166c91f2283afe9a0162df
ocawthorne <ocawthorne@gmail.com>
2020-10-28 15:41:19 +0000

    sql-aggregate-102920

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,17 +1,9 @@
-INSERT INTO students (name, grade, gpa, tardies)
-    VALUES ("John", 11, 3.4, 3);
-INSERT INTO students (name, grade, gpa, tardies)
-    VALUES ("Kareem", 10, 2.4, 12);
-INSERT INTO students (name, grade, gpa, tardies)
-    VALUES ("Zak", 12, 4.0, 0);
-INSERT INTO students (name, grade, gpa, tardies)
-    VALUES ("Kim", 11, 2.0, 9);
-INSERT INTO students (name, grade, gpa, tardies)
-    VALUES ("Bob", 10, 1.9, 17);
-INSERT INTO students (name, grade, gpa, tardies)
-    VALUES ("Stef", 9, 2.5, 2);
-INSERT INTO students (name, grade, gpa, tardies)
-    VALUES ("George", 9, 3.9, 1);
-INSERT INTO students (name, grade, gpa, tardies)
-    VALUES ("Daryl", 9, 3.5, 5);
-    
+INSERT INTO students (name, grade, gpa, tardies) VALUES
+("John", 11, 3.4, 3),
+("Kareem", 10, 2.4, 12),
+("Zak", 12, 4.0, 0),
+("Kim", 11, 2.0, 9),
+("Bob", 10, 1.9, 17),
+("Stef", 9, 2.5, 2),
+("George", 9, 3.9, 1),
+("Daryl", 9, 3.5, 5);
